342b61ce4e3267daa3b304d05bf323e3bc3c6353
Brian Berns <brianberns@gmail.com>
2025-05-04 00:36:07 -0400

    More work on F# translation.

--- ParticleLenia/Program.fs
+++ ParticleLenia/Program.fs
@@ -1,5 +1,10 @@
 ﻿open DiffSharp
 
+/// Computes the value of a Gaussian peak at the given point with
+/// the given mean and standard deviation.
+let peak_f (x : Tensor) (mu : float (*mean*)) (sigma : float (*std dev*)) =
+    exp(-((x - mu) / sigma) ** 2)
+
 let mu_k = 4.0
 let sigma_k = 1.0
 let w_k = 0.022
@@ -7,28 +12,15 @@ let mu_g = 0.6
 let sigma_g = 0.15
 let c_rep = 1.0
 
-/// Computes the value of a Gaussian peak at the given point with
-/// the given mean and standard deviation.
-let peak_f (x : Tensor) (mu : Tensor (*mean*)) (sigma : Tensor (*std dev*)) =
-    exp(-((x - mu) / sigma) ** 2)
-
 let square x = x * x
 
 let fields_f (points : Tensor) (x : Tensor) =
-  let r =
-    sqrt(
-        square(x - points)
-            .sum(-1)
-            .clamp(1e-10))
-  (*
-  U = peak_f(r, p.mu_k, p.sigma_k).sum()*p.w_k
-  G = peak_f(U, p.mu_g, p.sigma_g)
-  R = p.c_rep/2 * ((1.0-r).clip(0.0)**2).sum()
-  return Fields(U, G, R, E=R-G)
-  *)
-  ()
+    let r = sqrt(square(x-points).sum(-1).clamp(1e-10))
+    let U = (peak_f r mu_k sigma_k).sum()*w_k
+    let G = peak_f U mu_g sigma_g
+    let R = c_rep/2.0 * ((1.0-r).clamp(0.0)**2).sum()
+    {| U=U; G=G; R=R; E=R-G|}
 
 let points0 =
     (dsharp.rand([200; 2]) - 0.5) * 12.0
-printfn "%A" points0
 let dt = 0.1
