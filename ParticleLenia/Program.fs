b4a85e9ccbd815ed54ae5717b9137748f63f4d60
Brian Berns <brianberns@gmail.com>
2025-05-03 23:37:27 -0400

    Began work on F# translation.

---
+++ ParticleLenia/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.Distributions
+
+let parms =
+    {|
+        mu_k = 4.0
+        sigma_k = 1.0
+        w_k = 0.022
+        mu_g = 0.6
+        sigma_g = 0.15
+        c_rep = 1.0
+    |}
+
+let points0 : Matrix<float> =
+    DenseMatrix.random 200 2 (ContinuousUniform(-6., 6.))
+printfn "%A" points0
