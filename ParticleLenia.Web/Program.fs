fdc41d61f2b1684153933fa1810e808d159e9570
Brian Berns <brianberns@gmail.com>
2025-05-06 23:45:48 -0400

    More work on Fable implementation.

--- ParticleLenia.Web/Program.fs
+++ ParticleLenia.Web/Program.fs
@@ -30,30 +30,35 @@ module Program =
     let steps_per_frame = 10
     let world_width = 25.0
 
-    let rec animate points =
+    let animate points =
 
         let points, fields =
             ((points, Array.empty), [1 .. steps_per_frame])
                 ||> Seq.fold (fun (points, _) _ ->
                     Engine.step settings points)
 
-        let width = ctx.canvas.width
-        let height = ctx.canvas.height
-        // ctx.resetTransform()
+        let width = canvas.width
+        let height = canvas.height
         ctx.clearRect(0, 0, width, height)
         ctx.translate(width / 2.0, height / 2.0)
-        let s = width/world_width
+        let s = width / world_width
         ctx.scale(s, s)
         ctx.lineWidth <- 0.1
         for i = 0 to points.Length - 1 do
             ctx.beginPath()
             let pt = points[i]
             let r = settings.c_rep / (fields[i].R_val * 5.0)
-            ctx.arc(pt.X, pt.Y, r, 0.0, Math.PI)
+            ctx.arc(pt.X, pt.Y, r, 0.0, Math.PI * 2.0)
             ctx.stroke()
+        // ctx.resetTransform()
+        ctx.setTransform(1.0, 0.0, 0.0, 1.0, 0.0, 0.0)
 
-        window.requestAnimationFrame(fun timestamp ->
-            animate points)
+        points
+
+    let rec loop points =
+        window.requestAnimationFrame(fun _timestamp ->
+            animate points
+                |> loop)
                 |> ignore
 
     let points =
@@ -63,6 +68,4 @@ module Program =
         Array.init nPoints (fun _ ->
             { X = coord (); Y = coord () })
 
-    window.requestAnimationFrame(fun timestamp ->
-        animate points)
-            |> ignore
+    loop points
